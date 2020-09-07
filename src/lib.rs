7335b744beee775e4735b76786bb82186f6b3264
Jack Gerrits <jack@jackgerrits.com>
2020-09-07 12:49:40 -0400

    Update submodule and fix used functions

--- src/lib.rs
+++ src/lib.rs
@@ -18,19 +18,19 @@ mod tests {
     #[test]
     fn test_initialize_and_parse_learn_example() {
         unsafe {
-            let errString = vw_create_error_string();
+            let errString = vw_create_error_info();
             let mut options = mem::MaybeUninit::uninit();
             let result = vw_create_options(options.as_mut_ptr(), errString);
-            assert_eq!(result, VW_SUCCESS);
+            assert_eq!(result, VW_success);
 
             let options = options.assume_init();
             let command_line_str = CString::new("--quiet").unwrap();
             let result = vw_options_set_bool(options, command_line_str.as_ptr(), true, errString);
-            assert_eq!(result, VW_SUCCESS);
+            assert_eq!(result, VW_success);
 
             let result = vw_destroy_options(options, errString);
-            assert_eq!(result, VW_SUCCESS);
-            vw_destroy_error_string(errString);
+            assert_eq!(result, VW_success);
+            vw_destroy_error_info(errString);
         }
     }
 }
